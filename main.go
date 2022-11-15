8017786f8cc3c4aaca0f0f748d50bb2a09a84b82
OkGrowInc <john@okgrow.com>
2022-11-15 14:49:33 -0600

    edits

--- main.go
+++ main.go
@@ -1,42 +1,64 @@
 package main
 
-import(
-"fmt"
-"os"
-"encoding/json"
+import (
+	"encoding/json"
+	"fmt"
 )
 
-type Address struct{
-	City string
-	State string
+type Address struct {
+	City    string
+	State   string
 	Country string
 	Pincode json.Number
 }
 
-type User struct{
-	Name string
-	Age json.Number
+type User struct {
+	Name    string
+	Age     json.Number
 	Contact string
 	Company string
 	Address Address
 }
 
-func main(){
+func main() {
 	dir := "./"
 
 	db, err := New(dir, nil)
 	if err != nil {
 		fmt.Println("Error", err)
-				}
-}
+	}
 
 	employees := []User{
 		{"John", "55", "23344333", "OS Intelligent", Address{"Overland Park", "Kansas", "United States of America", "410013"}},
 		{"Jodi", "53", "23344333", "OS Intelligent", Address{"Pratt", "Kansas", "United States of America", "67120"}},
-}
+	}
 
-for _, value := range employees{
-	db.Write("users", value.Name, User{
+	for _, value := range employees {
+		db.Write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
 
-	})
-}
+	records, err := db.ReadAll("users")
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	fmt.Println(records)
+
+	allusers := []User{}
+
+	for _, f := range records {
+		employeeFound := User{}
+		if err := json.Unmarshal([]byte(f), &employeeFound); err != nil {
+			fmt.Println("Error", err)
+		}
+		allusers = append(allusers, employeeFound)
+	}
+}	 
+fmt.Println((allusers))
+
+db.Delete("user", "john")1 err !=
